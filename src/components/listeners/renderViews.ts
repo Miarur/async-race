ccd5dad5a90139629d3867f17dce3d10e57764c6
miarur <deadheartedfo@yahoo.com>
2022-08-11 21:35:32 +0300

    feat: add render views

---
+++ src/components/listeners/renderViews.ts
@@ -0,0 +1,24 @@
+import  { store } from '../../store';
+import { renderTable } from '../renders/renders';
+import { renderGarage } from '../renders/renderGarage';
+import { updateStore } from '../pagination/pagination';
+
+export const renderViews = async () => {
+  const viewWinners = document.querySelector('.winners') as HTMLButtonElement;
+  const viewGarage = document.querySelector('.garage') as HTMLButtonElement;
+  const app = document.getElementById('app') as HTMLElement; 
+
+  viewWinners?.addEventListener('click', (event: Event) => {
+    event.preventDefault(); 
+    store.view = 'winners'; 
+    app.innerHTML = `${renderTable()}`; 
+    updateStore(); 
+  });  
+
+  viewGarage?.addEventListener('click', (event: Event) => {
+    event.preventDefault(); 
+    store.view = 'garage'; 
+    app.innerHTML = `${renderGarage()}`; 
+    updateStore(); 
+  });  
+}
